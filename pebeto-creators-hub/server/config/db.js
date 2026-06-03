fd7dab936f66cc4e1cdd9b181175ba615335643b
Dirblair <pebetostudiosafrica@gmail.com>
2026-06-03 22:49:32 +0300

    Update db.js

--- pebeto-creators-hub/server/config/db.js
+++ pebeto-creators-hub/server/config/db.js
@@ -1,9 +1,18 @@
 const mongoose = require('mongoose');
 
 async function connectDB(uri) {
-  mongoose.set('strictQuery', true);
-  await mongoose.connect(uri);
-  console.log('MongoDB connected');
+  try {
+    mongoose.set('strictQuery', true);
+    console.log('DEBUG: Attempting to connect to MongoDB...');
+    
+    await mongoose.connect(uri);
+    
+    console.log('MongoDB connected successfully');
+  } catch (error) {
+    console.error('CRITICAL DATABASE ERROR:', error.message);
+    // Exit process with failure code so Render knows it crashed
+    process.exit(1);
+  }
 }
 
 module.exports = { connectDB };
