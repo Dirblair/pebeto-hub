dc68a5bd2638ec561394d46d3c4bb4375d72dce9
Dirblair <pebetostudiosafrica@gmail.com>
2026-06-12 08:42:14 +0300

    Create Comment.js

---
+++ pebeto-creators-hub/server/models/Comment.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const commentSchema = new mongoose.Schema({
+  creatorId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  author: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  text: { type: String, required: true, maxlength: 500 },
+  createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Comment', commentSchema);
