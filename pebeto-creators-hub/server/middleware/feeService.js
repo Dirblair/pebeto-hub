7a771aa6e6e1ea8d70c43a8dd13d51866bc032f9
Dirblair <pebetostudiosafrica@gmail.com>
2026-06-13 13:10:35 +0300

    Update feeService.js

--- pebeto-creators-hub/server/middleware/feeService.js
+++ pebeto-creators-hub/server/middleware/feeService.js
@@ -2,6 +2,8 @@
  * Fee Service Middleware for Pebeto Creator's Hub
  * 
  * Attaches fee calculation functions to every request object.
+ * This middleware imports the core fee calculation logic from services/feeService.js
+ * and makes it available as req.feeService in all route handlers.
  * 
  * @module middleware/feeService
  */
@@ -10,30 +12,51 @@ const feeService = require('../services/feeService');
 
 /**
  * Express middleware to attach fee helpers to req for route handlers
+ * @param {Object} req - Express request object
+ * @param {Object} res - Express response object
+ * @param {Function} next - Express next function
  */
 function attachFeeService(req, res, next) {
+  // Make sure feeService exists and has all required functions
+  if (!feeService) {
+    console.error('❌ feeService not loaded correctly');
+    return next(new Error('Fee service not available'));
+  }
+
   req.feeService = {
+    // Deposit calculations
     calculateDeposit: feeService.calculateDeposit,
     previewDeposit: feeService.previewDeposit,
+    
+    // Tip calculations
     calculateTip: feeService.calculateTip,
     previewTip: feeService.previewTip,
+    
+    // Withdrawal calculations
     calculateWithdrawal: feeService.calculateWithdrawal,
     previewWithdrawal: feeService.previewWithdrawal,
     validateMinimumWithdrawal: feeService.validateMinimumWithdrawal,
     validateWithdrawalRequest: feeService.validateWithdrawalRequest,
+    
+    // Currency conversion
     convertLocalToUsd: feeService.convertLocalToUsd,
     convertUsdToLocal: feeService.convertUsdToLocal,
     resolveWithdrawalAmountUsd: feeService.resolveWithdrawalAmountUsd,
     getExchangeRate: feeService.getExchangeRate,
+    
+    // Rounding utilities
     roundUsd: feeService.roundUsd,
     roundAmount: feeService.roundAmount,
     validateAmount: feeService.validateAmount,
     formatCurrency: feeService.formatCurrency,
+    
+    // Constants
     MIN_WITHDRAWAL_USD: feeService.MIN_WITHDRAWAL_USD,
     FEE_RATES: feeService.FEE_RATES
   };
+  
   next();
 }
 
-// Make sure this exports a function (not an object)
+// Export the middleware function directly (not as an object)
 module.exports = attachFeeService;
