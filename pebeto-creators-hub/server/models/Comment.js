f9549c8db26b8566137accee4d26230dacc3e87a
Dirblair <pebetostudiosafrica@gmail.com>
2026-06-17 11:31:43 +0300

    Update Comment.js

--- pebeto-creators-hub/server/models/Comment.js
+++ pebeto-creators-hub/server/models/Comment.js
@@ -7,4 +7,4 @@ const commentSchema = new mongoose.Schema({
   createdAt: { type: Date, default: Date.now }
 });
 
-module.exports = mongoose.model('Comment', commentSchema);
+module.exports = mongoose.model('Comment', commentSchem
